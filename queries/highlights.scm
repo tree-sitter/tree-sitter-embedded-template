2d1cbed7bdc765d811da24a9fd0d4b196edf07dc
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-10-17 15:05:52 -0700

    Add highlight and injection queries (#2)
    * Add highlight and injection queries
    
    * :fire: property sheets

---
+++ queries/highlights.scm
@@ -0,0 +1,10 @@
+(comment_directive) @comment
+
+"<%#" @keyword
+"<%" @keyword
+"<%=" @keyword
+"<%_" @keyword
+"<%-" @keyword
+"%>" @keyword
+"-%>" @keyword
+"_%>" @keyword
---
+++ queries/injections-ejs.scm
@@ -0,0 +1,7 @@
+((template
+  (content) @injection.content) @injection.site
+ (set! injection.language "html"))
+
+((template
+  (* (code) @injection.content)) @injection.site
+ (set! injection.language "javascript"))
