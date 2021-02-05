91b5100ab5be9b5e5acac653657d0ae647e215c0
Arthur Baars <aibaars@github.com>
2021-02-05 18:51:32 +0100

    Rust: no longer compile scanner.cc and update dependencies

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -14,15 +14,4 @@ fn main() {
     c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
     c_config.compile("parser");
-
-    let mut cpp_config = cc::Build::new();
-    cpp_config.cpp(true);
-    cpp_config.include(&src_dir);
-    cpp_config
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable");
-    let scanner_path = src_dir.join("scanner.cc");
-    cpp_config.file(&scanner_path);
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    cpp_config.compile("scanner");
 }
