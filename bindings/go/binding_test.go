4f7b7e76c6e2b9fe95af8a644d4aaad21c93d6f8
Amaan Qureshi <amaanq12@gmail.com>
2024-09-02 02:09:08 -0400

    build: update bindings

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,13 +3,13 @@ package tree_sitter_embedded_template_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 	tree_sitter_embedded_template "github.com/tree-sitter/tree-sitter-embedded-template/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_embedded_template.Language())
 	if language == nil {
-		t.Errorf("Error loading Embedded Template grammar")
+		t.Errorf("Error loading EmbeddedTemplate grammar")
 	}
 }
--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,9 +1,11 @@
-//! This crate provides Embedded Template language support for the [tree-sitter][] parsing library.
+//! This crate provides EmbeddedTemplate language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
+//! use tree_sitter::Parser;
+//!
 //! let code = r#"
 //! <html>
 //!  <body>
@@ -13,8 +15,11 @@
 //! </body>
 //! </html>
 //! "#;
-//! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_embedded_template::language()).expect("Error loading Embedded Template grammar");
+//! let mut parser = Parser::new();
+//! let language = tree_sitter_embedded_template::LANGUAGE;
+//! parser
+//!     .set_language(&language.into())
+//!     .expect("Error loading EmbeddedTemplate parser");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
@@ -24,18 +29,14 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_embedded_template() -> Language;
+    fn tree_sitter_embedded_template() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
-///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_embedded_template() }
-}
+/// The tree-sitter [`LanguageFn`] for this grammar.
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_embedded_template) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -57,7 +58,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language())
-            .expect("Error loading Embedded Template grammar");
+            .set_language(&super::LANGUAGE.into())
+            .expect("Error loading EmbeddedTemplate parser");
     }
 }
